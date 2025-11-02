5a1b24b14be562d6a3ab641bc93882ae8216aab4
Persefoni Koutridi <koutridi.persefoni@gmail.com>
2025-11-02 03:05:06 +0200

    initial commit

---
+++ src/test/java/com/library/librarymanagementsystemapi/LibraryManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.librarymanagementsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
